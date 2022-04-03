c534806f28d9ef4139f3fcf15a6daa5da247b828
Alan <lialan123@gmail.com>
2022-04-02 17:06:03 -0700

    Cleaning up the code some more.

--- madlibs.py
+++ madlibs.py
@@ -5,16 +5,6 @@ import string
 import nltk
 import os
 
-# All strings beginning with "_" are input signs.
-# @PROPN = proper noun
-# _VRB = verb 
-# _ADJ = adjective
-# _N = singular noun
-# _PLN = plural noun
-# _ING = verb ending in "-ing"
-# _NUM = number
-# _POSSN = possessive noun
-
 def main():
 	finalStory = "" # The string that will contain the final version of the mad lib.
 	lineList = [] # List that will contain all the lines in the text.
@@ -22,34 +12,34 @@ def main():
 		for line in file:
 			lineList.append(line)
 	#randStory(lineList)
-	fillStory(finalStory, lineList)
+	finalStory = fillStory(lineList)
+	print(finalStory)
 
-def fillStory(result, lineList):
-	lastPunc = " "
+def fillStory(lineList):
+	result = ""
 	count = 0 # Keep track of how many lines have been gone through.
-	for line in lineList:
-		for word in line.split():
-			question = "" # Always reset the question
-			wordToAdd = word
-			if lastPunc != " ":
-				result += " "
-			lastPunc = " " # Reset the last punctuation mark
-			if word[0] == "_":
-				check = word[len(word) - 1]
-				if check == "." or check == "?" or check == "!" or check == ",":
-					lastPunc = check
-			question = whichQuestion(word)
-			if question != "": # No input is asked for if the question is blank
-				wordToAdd = input(question)
-			result += wordToAdd + lastPunc # Add the word and either a space or a punctuation mark to the final story.
+	for i in lineList:
+		tokens = nltk.word_tokenize(i)
+		tagged = nltk.pos_tag(tokens)
 		count += 1
+		firstWord = True;
+		print(tagged)
+		for j in tagged:
+			question = whichQuestion(j[0])
+			wordToAdd = j[0]
+			if isPunc(j) == False and firstWord == False: ## Add a space if the word is not punctuation, and it is not the first word.
+				result += " "
+			if question != "": # If there is no question, just add the word to the result.
+				wordToAdd = input(question)
+			result += wordToAdd
+			firstWord = False
 		if count != len(lineList): # Do not add a new line after we are done with all the lines.
 			result += "\n" # Start a new line after the current line is done
-		lastPunc = " "
-	print(result)
+		firstWord = True
+	return result
 
 replaceTags = ["JJ", "JJR", "JJS", "NN", "NNP", "NNS", "RB", "RBR", "RBS", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
-noReplaceTags = ["CC", "CD", "DT", "EX", "IN", "LS", "MD", "PDT", "POS", "PRP", "RP", "TO", "WDT", "WP", "WRB"]
+noReplaceTags = ["CC", "CD", "DT", "EX", "IN", "LS", "MD", "PDT", "POS", "PRP", "PRP$", "RP", "TO", "WDT", "WP", "WRB"]
 
 def randStory(lineList):
 	try:
@@ -61,29 +51,33 @@ def randStory(lineList):
 			tokens = nltk.word_tokenize(i)
 			tagged = nltk.pos_tag(tokens)
 			for j in tagged:
+				if isPunc(j) == True:
+					print(j[0])
 				if j[1] not in replaceTags:
 					f.write(j[0] + " ")
-				else:
-					print(j)
-					print("Hello, world!")
 			f.write("\n")
 
+# Check whether the word has any punctuation in it.
+def isPunc(word):
+	if "'" in word[0] or (word[1] not in replaceTags and word[1] not in noReplaceTags):
+		return True
+	return False
+
+# Return a question response if the word is to be replaced.
 def whichQuestion(word):
 	question = ""
-	if "_PROPN" in word:
+	if "NN" == word:
+		question = "Name a singular noun: "
+	elif "NNP" == word:
 		question = "Name a proper noun: "
-	elif "_VRB" in word:
-		question = "Name a verb: "
-	elif "_ADJ" in word:
-		question = "Name an adjective: "
-	elif "_NOUN" in word:
-		question = "Name a noun: "
-	elif "_PLN" in word:
+	elif "NNS" == word:
 		question = "Name a plural noun: "
-	elif "_ING" in word:
+	elif "VB" == word:
+		question = "Name a verb: "
+	elif "VBG" == word:
 		question = "Name an -ing word: "
-	elif "_POSSN" in word:
-		question = "Name a possessive noun: "
+	elif "JJ" == word:
+		question = "Name an adjective: "
 	return question
 
 if __name__ == "__main__":
