2c57e18cae3d962a78ae4b4be72e97582b042622
Alan <lialan123@gmail.com>
2022-04-02 21:55:29 -0700

    Saving more work on randomizer.

--- madlibs.py
+++ madlibs.py
@@ -4,18 +4,37 @@ import time
 import string
 import nltk
 import os
+import random
 
 def main():
 	finalStory = "" # The string that will contain the final version of the mad lib.
 	lineList = [] # List that will contain all the lines in the text.
-	with open("easy/testfile1.txt") as file:
-		for line in file:
-			lineList.append(line)
-	#randStory(lineList)
-	finalStory = fillStory(lineList)
+	randomizer = False
+	print("Welcome to Mad Libs Generator.\n"\
+		"There are two modes: Easy Mode and Random Mode.\n"\
+		"In Easy Mode, you fill in spots already decided for you.\n"\
+		"In Random Mode, you fill in spots that will be randomly chosen."\
+	   )
+	while True:
+		answer = input("Would you like to play Easy Mode? Say Yes or No.\n")
+		if answer.lower() == "yes":
+			randomizer = False
+			with open("easy/testfile1.txt") as file:
+				for line in file:
+					lineList.append(line)
+			break
+		elif answer.lower() == "no":
+			randomizer = True
+			with open("random/testfile1.txt") as file:
+				for line in file:
+					lineList.append(line)
+			break
+		else:
+			print("Please try again.")
+	finalStory = fillStory(lineList, randomizer)
 	print(finalStory)
 
-def fillStory(lineList):
+def fillStory(lineList, randomizer):
 	result = ""
 	count = 0 # Keep track of how many lines have been gone through.
 	for i in lineList:
@@ -23,14 +42,19 @@ def fillStory(lineList):
 		tagged = nltk.pos_tag(tokens)
 		count += 1
 		firstWord = True;
-		print(tagged)
 		for j in tagged:
-			question = whichQuestion(j[0])
 			wordToAdd = j[0]
 			if isPunc(j) == False and firstWord == False: ## Add a space if the word is not punctuation, and it is not the first word.
 				result += " "
+			if randomizer == True and j[1] in replaceTags: # Only randomize the word half the time.
+				randNum = random.randint(1, 10)
+				if randNum <= 5:
+					wordToAdd = j[1] # Set the word to a tag to be replaced afterwards.
+			question = whichQuestion(wordToAdd) # Ask for the user's input to replace the tag with.
 			if question != "": # If there is no question, just add the word to the result.
 				wordToAdd = input(question)
+			if firstWord == True:
+				wordToAdd = wordToAdd.capitalize()
 			result += wordToAdd
 			firstWord = False
 		if count != len(lineList): # Do not add a new line after we are done with all the lines.
@@ -40,22 +64,13 @@ def fillStory(lineList):
 
 replaceTags = ["JJ", "JJR", "JJS", "NN", "NNP", "NNS", "RB", "RBR", "RBS", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
 noReplaceTags = ["CC", "CD", "DT", "EX", "IN", "LS", "MD", "PDT", "POS", "PRP", "PRP$", "RP", "TO", "WDT", "WP", "WRB"]
-
+"""
 def randStory(lineList):
 	try:
 		os.remove("testcase.txt")
 	except OSError:
 		pass
-	with open("testcase.txt", "x") as f:
-		for i in lineList:
-			tokens = nltk.word_tokenize(i)
-			tagged = nltk.pos_tag(tokens)
-			for j in tagged:
-				if isPunc(j) == True:
-					print(j[0])
-				if j[1] not in replaceTags:
-					f.write(j[0] + " ")
-			f.write("\n")
+"""
 
 # Check whether the word has any punctuation in it.
 def isPunc(word):
@@ -74,6 +89,7 @@ def whichQuestion(word):
 		question = "Name a plural noun: "
 	elif "VB" == word:
 		question = "Name a verb: "
+	elif ""
 	elif "VBG" == word:
 		question = "Name an -ing word: "
 	elif "JJ" == word:
